8c17aec2c87888b864fadb509503d75658d9d658
Karam-Kassam-Alhinnawi <storiesmorekaram@gmail.com>
2025-11-15 21:39:17 +0300

    fixing GH problems

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
